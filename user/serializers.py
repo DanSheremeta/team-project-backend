578cf06317c3476d577a33ed6ba13b65d747d7de
DanSheremeta <danylo3002@gmail.com>
2024-06-03 19:09:43 +0300

    Add first_name and last_name fields instead of name field in User model.

--- user/serializers.py
+++ user/serializers.py
@@ -8,7 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "email",
-            "name",
+            "first_name",
+            "last_name",
             "password",
             "is_staff"
         )
